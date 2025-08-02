6c6f7aae2c57ee94e1dd280c628e3f5ed07157e4
unknown <rudrashishpalav@gmail.com>
2025-08-02 22:56:40 +0200

    javascript searches for your texts woohoo

--- language.js
+++ language.js
@@ -1,4 +1,14 @@
 // find DOM object that corresponds to job desc
+const jobDesc = document.getElementById("job-details");
+if(jobDesc) {
+    console.log("jobDesc found");
+    var text = [];
+    jobDesc.childNodes.forEach((node) => {
+        text.push(node.textContent);
+    });
+    console.log(text);
+
+};
 
 // compare to list of language names ie german, deutsch, english, etc
 
