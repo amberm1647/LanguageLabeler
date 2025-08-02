aa6007dcfc376acc819a2becd92658edb31deddd
Amber Mao <amber.m1647@gmail.com>
2025-08-02 12:24:33 -0700

    initial commit:

---
+++ language.js
@@ -0,0 +1,11 @@
+// follow link from linkedin to external job posting
+
+// find DOM object that corresponds to job desc
+
+// compare to list of language names ie german, deutsch, english, etc
+
+// parse w/ regex and find language keywords
+
+// create html/css for label to add
+
+// create instance of label, add to DOM
