a57db73a40c25d5a762526b248dd413b6271a2e1
Simone Carsey <72096779+carseys@users.noreply.github.com>
2025-08-03 11:05:07 +0200

    waiting introduced to function

--- language.js
+++ language.js
@@ -1,14 +1,45 @@
 // find DOM object that corresponds to job desc
-const jobDesc = document.getElementById("job-details");
-if(jobDesc) {
-    console.log("jobDesc found");
-    var text = [];
-    jobDesc.childNodes.forEach((node) => {
-        text.push(node.textContent);
-    });
-    console.log(text);
 
-};
+async function waiting() {
+    console.log("Before sleep");
+    await sleep(10000); // Sleep for 5 seconds
+    console.log("After sleep [After 10 Seconds]");
+    return true;
+}
+  
+async function sleep(ms) {
+    await new Promise((resolve) => setTimeout(resolve, ms));
+}
+  
+  
+async function getJobDesc() {
+    const waited = await waiting()
+    console.log("Waiting finished: ", waited);
+    const jobDesc = document.getElementById("job-details");
+    if(jobDesc) {
+        console.log("jobDesc found");
+        var text = [];
+        jobDesc.childNodes.forEach((node) => {
+            text.push(node.textContent);
+        });
+        console.log(text);
+
+    };
+}
+
+// waiting();
+getJobDesc();
+
+// const jobDesc = document.getElementById("job-details");
+// if(jobDesc) {
+//     console.log("jobDesc found");
+//     var text = [];
+//     jobDesc.childNodes.forEach((node) => {
+//         text.push(node.textContent);
+//     });
+//     console.log(text);
+
+// };
 
 // compare to list of language names ie german, deutsch, english, etc
 
